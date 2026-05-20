570a21b7b7a08f846f25f9f99fe2c3b209a26a72
Asraful Alam <asrafulalam.origin@gmail.com>
2026-05-21 05:46:54 +0600

    add featured tutors data to the home route

--- index.js
+++ index.js
@@ -22,42 +22,46 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-
-    db = client.db('mediqueue')
+    const db = client.db('mediqueue')
     const tutorCollection = db.collection('tutor')
-
     app.post('/add-tutor', async (req, res) => {
       const tutorsData = req.body;
-
-      console.log(tutorsData)
-      
+      // console.log(tutorsData)
       const result = await tutorCollection.insertOne(tutorsData)
-
       res.json(result)
     })
-
-
-  
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-   
-    // await client.close();
-
+      // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  try{
+    const db = client.db('mediqueue');
+    const tutorCollection = db.collection('tutor');
+
+    const tutors = await tutorCollection.aggregate([
+      { $limit: 6}
+    ]).toArray();
+    res.json(tutors);
+  } catch(error){
+    console.error(error);
+    res.status(500).json({ message: 'Failed to fetch featured tutors'})
+  }
 })
 
-app.get('/tutors', (req, res) => {
-    res.send('tutors are showing')
-})
 
+app.get('/tutors',  (req, res) => {
+   res.send('tutors data is coming');
+})
+app.get('/add-tutor', (req, res) => {
+  res.send('tutors are adding');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
