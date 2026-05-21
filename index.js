def04eed96003120d91db987cee46e89390a276d
Asraful Alam <asrafulalam.origin@gmail.com>
2026-05-21 13:16:00 +0600

    change the routing layout and send filtered data to tutors page

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
-const app = express()
+const app = express();
 const port = process.env.PORT;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,59 +17,75 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     await client.connect();
-    const db = client.db('mediqueue')
-    const tutorCollection = db.collection('tutor')
+    const db = client.db('mediqueue');
+    const tutorCollection = db.collection('tutor');
+
+    app.get('/featured-tutors', async (req, res) => {
+      try {
+        const tutors = await tutorCollection.aggregate([
+          { $limit: 6 }
+        ]).toArray();
+        res.json(tutors);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch featured tutors' });
+      }
+    });
+
+    
+    app.get('/tutors', async (req, res) => {
+      try {
+        const { search, subject, mode } = req.query;
+
+        
+        const query = {};
+
+        if (subject) {
+          query.subject = subject;
+        }
+
+  
+        if (mode) {
+          query.mode = mode;
+        }
+
+
+        const tutors = await tutorCollection.find(query).toArray();
+        res.json(tutors);
+
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch all tutors' });
+      }
+    });
+
+    app.get('/add-tutor', (req, res) => {
+      res.send('tutors are adding');
+    });
+
     app.post('/add-tutor', async (req, res) => {
       const tutorsData = req.body;
-      // console.log(tutorsData)
-      const result = await tutorCollection.insertOne(tutorsData)
-      res.json(result)
-    })
+      const result = await tutorCollection.insertOne(tutorsData);
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
-      // await client.close();
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('Server Running');
+});
 
-
-app.get('/', async (req, res) => {
-  try{
-    const db = client.db('mediqueue');
-    const tutorCollection = db.collection('tutor');
-
-    const tutors = await tutorCollection.aggregate([
-      { $limit: 6}
-    ]).toArray();
-    res.json(tutors);
-  } catch(error){
-    console.error(error);
-    res.status(500).json({ message: 'Failed to fetch featured tutors'})
-  }
-})
-
-
-app.get('/tutors', async (req, res) => {
-    try {
-    const db = client.db('mediqueue');
-    const tutorCollection = db.collection('tutor');
-
-    const tutors = await tutorCollection.find().toArray();
-
-    res.json(tutors);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Failed to fetch all tutors' });
-  }
-})
-app.get('/add-tutor', (req, res) => {
-  res.send('tutors are adding');
-})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
